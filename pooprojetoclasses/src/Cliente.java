70c1b048b018853bff9bd9d8d43534cfe90920a9
Patrick <patrickmullerjp@hotmail.com>
2017-03-05 21:45:56 -0300

    classesdoprojeto

---
+++ pooprojetoclasses/src/Cliente.java
@@ -0,0 +1,27 @@
+
+public class Cliente {
+
+	
+	private String nome;
+	private String CPF;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public String toString() {
+		return "Cliente [Nome=" + nome + ", CPF=" + CPF + "]";
+	}
+	
+	
+	
+}
